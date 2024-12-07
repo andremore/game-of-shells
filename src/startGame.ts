1d1665d897fe1ec20e4ea172615fe30a5e375c0b
andremore <andremorgado.work@gmail.com>
2024-12-07 15:53:42 +0000

    feat: inital game logic

--- src/startGame.ts
+++ src/startGame.ts
@@ -1,4 +1,4 @@
-import { settings } from "./state";
+import { gameState, resetGameState, setBallIndex, setShellsClickHandlers, settings } from "./state";
 import { gameIrrelevantElementIds } from "./types/constants";
 
 function destroyGameIrrelevantElements() {
@@ -9,6 +9,17 @@ function destroyGameIrrelevantElements() {
     }
 }
 
+const shellClickHandler = (index: number) => {
+    if (index === gameState.ballIndex) {
+        resetGameState();
+        console.log('you won');
+        return;
+    }
+
+    console.log('you lost');
+    resetGameState();
+}
+
 function createGameShells(container: HTMLDivElement) {
     const shellContainer = document.createElement('div');
     shellContainer.className = 'shell-container';
@@ -17,14 +28,49 @@ function createGameShells(container: HTMLDivElement) {
         const shell = document.createElement('div');
 
         shell.classList.add('shell');
-        shell.addEventListener('click', () => console.log(`${i} shell clicked`));
         shellContainer.appendChild(shell);
+        gameState.shells.push({
+            element: shell,
+            listener: () => shellClickHandler(i),
+            handlerFn: () => null
+        });
     }
 
     container.appendChild(shellContainer);
 }
 
+function showBallInShellTemporarily(): Promise<void> {
+    if (gameState.ballIndex == null) {
+        return Promise.resolve();
+    }
+
+    const ball = document.createElement('div');
+    ball.id = 'ball';
+
+    const shellToAddBall = gameState.shells[gameState.ballIndex].element;
+
+    return new Promise<void>(resolve => {
+        setTimeout(() => {
+            shellToAddBall.appendChild(ball);
+
+            setTimeout(() => {
+                ball.remove();
+                resolve();
+            }, 1500);
+        }, 250);
+    });
+};
+
+const shuffleShells = () => {
+    console.log('shuffle');
+}
+
 export function startGame(container: HTMLDivElement) {
     destroyGameIrrelevantElements();
     createGameShells(container);
+    setBallIndex();
+    showBallInShellTemporarily().then(() => {
+        shuffleShells();
+        setShellsClickHandlers();
+    });
 }
--- src/state.ts
+++ src/state.ts
@@ -5,5 +5,31 @@ export const settings: Settings = defaultSettings;
 
 export const gameState: GameState = {
     shells: [],
-    ballIndex: null
+    ballIndex: null,
 };
+
+export function setBallIndex() {
+    gameState.ballIndex = Math.floor(Math.random() * gameState.shells.length);
+}
+
+export function setShellsClickHandlers() {
+    if (!gameState.shells.length) {
+        return;
+    }
+
+    for (let i = 0; i < gameState.shells.length; i++) {
+        const handlerFn = () => {
+            gameState.shells[i].listener(i);
+        };
+
+        gameState.shells[i].handlerFn = handlerFn;
+        gameState.shells[i].element.addEventListener('click', handlerFn);
+    }
+}
+
+export function resetGameState() {
+    let i = 0;
+    for (; i < gameState.shells.length; i++) {
+        gameState.shells[i].element.removeEventListener('click', gameState.shells[i].handlerFn);
+    }
+}
