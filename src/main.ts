166b2da7674b77d0ae5ec4367f3c736e5635afad
André Morgado <andremorgado.work@gmail.com>
2024-12-08 23:28:38 +0000

    fix: restart game and header buttons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { GameContainer } from './components/GameContainer';
 import { root } from './utils/constants';
 import './style.css';
 import { SettingsController } from './controllers/SettingsController';
+import { BtnRestart } from './components/Button/BtnRestart';
+import { gameStore } from './stores/gameStore';
+import { restartGame } from './game/gameLogic';
 
 function main() {
   const header = document.createElement('header');
@@ -12,7 +15,23 @@ function main() {
     SettingsController();
   });
 
+  const btnRestart = BtnRestart();
+  btnRestart.disabled = true;
+  btnRestart.addEventListener('click', () => {
+    if (gameStore.isGameOngoing) {
+      return;
+    }
+
+    restartGame(true);
+
+    document.getElementById('post-game-msg')?.remove();
+    document.getElementById('post-game-btn')?.remove();
+    btnRestart.disabled = true;
+  });
+  
+  
   header?.appendChild(btnSettings);
+  header?.appendChild(btnRestart);
   root?.appendChild(header);
 
   const main = document.createElement('main');
